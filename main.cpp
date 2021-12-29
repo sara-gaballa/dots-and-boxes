45402dd06c4a907c4a15e725f1d217399a1c47bc
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2021-12-30 01:04:15 +0200

    error fixed

--- main.cpp
+++ main.cpp
@@ -7,13 +7,7 @@
 #include"structs.h"
 #include"view.h"
 //#include"ai.h"
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
+
 bool changed;
 bool changed2;
 int s1,s2,s3,s4;
@@ -153,9 +147,14 @@ SDL_RenderPresent(renderer);
 LT=SDL_GetTicks();
 }
 }
-
 }
-
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
 int main(int argc, char* argv[]) {
 p1.color=0;//player one blue
 p2.color=1;//player2 green
@@ -168,8 +167,8 @@ printf(MAGENTA "\t \t \t [3] TOP TEN\n\n"RESET);
 printf(GREEN "\t \t \t [4] EXIT\n\n"RESET);
 printf("\n");
 
-int play , loadgame , top10 , Exit , hardness ,  num ,player1 ,player2  ,easy, hard ,dim;
-char input[2];
+int play , loadgame , top10 , Exit ,  player1 ,player2  ,easy=0, hard=0 , b , f ,g;
+char input[2] ,num[2] ,hardness[2] ,dim[2] ;
 printf(GREEN "ENTER 1 or 2 or 3 or 4:  " RESET);
 
 scanf("%s",&input);
@@ -197,17 +196,18 @@ int player1=0,player2=0;
  system("cls");
  printf(MAGENTA "\n\n\n\n\t \t \t[1]ONE PLAYER"RESET);
 printf(CYAN "\n\n\t \t \t[2]TWO PLAYER"RESET);
-scanf("%d",&num);
+scanf("%s",&num[0]);
 }
 
 while(1){
-if(num==1){
+int b = num[0]-'0';
+if(b==1){
 player1=1;
 printf(BLUE "\n ENTER YOUR NAME:" RESET);
 scanf("%s",&play1.name);
 break;}
 
-else if (num==2){
+else if (b==2){
 player2=1;
 printf(BLUE"\n ENTER YOUR NAME:"RESET);
 scanf("%s",&play1.name);
@@ -216,46 +216,68 @@ scanf("%s",&play2.name);
 break;
 }
 else { printf(RED "ERROR ENTER 1 or 2"RESET);
-    scanf("%d",&num);}
+    scanf("%s",&num[0]);}
 }
 
 system("cls");
 printf(GREEN"\n\n\n\n\t \t \t[1]EASY"RESET);
 printf(RED"\n\n\t \t \t[2]HARD"RESET);
-scanf("%d",&hardness);
+scanf("%s",&hardness[0]);
 while(1){
-if (hardness ==1 ){
+       int f=hardness[0]-'0';
+if (f ==1 ){
 easy=1;
 break;}
-else if(hardness ==2){
+else if(f ==2){
 hard=1;
 break;}
 else {
     printf(RED "ERROR ENTER 1 or 2"RESET);
-    scanf("%d",&hardness);
+    scanf("%s",&hardness[0]);
 }
 }
-printf("\n enter the dim (3 or 4 or 5) : ");
-scanf("%d",&N);
-if(N==3){
-    WIDTH=350,HEIGHT=350;
-}
-if(N==4){
-    WIDTH=550,HEIGHT=550;
-}
-if(N==5){
-    WIDTH=650,HEIGHT=650;
-}
+
+if(easy==1){
+      printf("\n enter the dim (3 or 4) : ");
+      scanf("%s",&dim[0]);
 while (1){
-if(N==3 || N==4 || N==5){
+       g = dim[0]-'0';
+if(g==3 || g==4){
+    if(g==3){
+    WIDTH=350,HEIGHT=350; N=3;break;}
+
+    if(g==4){
+    WIDTH=550,HEIGHT=550; N=4; break;}
 break;
 }
 else {
 printf(RED"ERROR"RESET);
-scanf("%d",&N);}
-}
+scanf("%s",&dim[0]);
+}}}
 
-while(hardness==1 && num==2){
+if(hard==1){
+      printf("\n enter the dim (5 or 6 or 7) : ");
+      scanf("%s",&dim[0]);
+    while(1){
+          g = dim[0]-'0';
+    if(g==5|| g==6 ||g==7){
+    if(g==5){
+    WIDTH=650,HEIGHT=650; N=5; break;}
+
+if(g==6){
+    WIDTH=750,HEIGHT=750; N=6; break;
+}
+if(g==7){
+    WIDTH=750,HEIGHT=750; N=7; break;
+}
+break;
+}
+else {
+printf(RED"ERROR"RESET);
+scanf("%s",&dim[0]);
+}
+}}
+while( player2==1){
 system("cls");
 view();
 //last=clock();
